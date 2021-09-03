62ab928b5c036549dd0fa4aae64a15ff5f5928fa
WooKyoung Noh <wookay.noh@gmail.com>
2021-09-04 04:21:00 +0900

    mergewith namedtuples

--- src/mergewith.jl
+++ src/mergewith.jl
@@ -1,22 +1,32 @@
 # module Lupin
 
-function mergewith_namedtuples(combine::Function, nt::NamedTuple, others::NamedTuple...)
-    for other in others
-        for (k,v) in pairs(other)
-            nt = merge(nt, (; k => haskey(nt, k) ? combine(nt[k], v) : v))
-        end
+using Base: merge_names, merge_types, sym_in
+
+mergewith_namedtuples(combine::Function, a::NamedTuple, b::NamedTuple, cs::NamedTuple...) = mergewith_namedtuples(combine, mergewith_namedtuples(combine, a, b), cs...)
+
+function mergewith_namedtuples(combine::Function, a::NamedTuple{an}, b::NamedTuple{bn}) where {an, bn}
+    names = merge_names(an, bn)
+    types = merge_types(names, typeof(a), typeof(b))
+    NamedTuple{names, types}(map(names) do n
+            if sym_in(n, bn)
+                if sym_in(n, an)
+                    combine(getfield(a, n), getfield(b, n))
+                else
+                    getfield(b, n)
+                end
+            else
+                getfield(a, n)
+            end
+        end)
+end
+
+if VERSION >= v"1.5.0-DEV.182"
+    function Base.mergewith(combine::Function, nt::NamedTuple, others::NamedTuple...)
+        mergewith_namedtuples(combine, nt, others...)
     end
-    return nt
-end
-
-using Jive
-@If VERSION >= v"1.5.0-DEV.182" function Base.mergewith(combine::Function, nt::NamedTuple, others::NamedTuple...)
-    mergewith_namedtuples(combine, nt, others...)
-end
-
-@If VERSION < v"1.5.0-DEV.182" begin
-export mergewith
-mergewith = mergewith_namedtuples
+else
+    export mergewith
+    mergewith = mergewith_namedtuples
 end
 
 # module Lupin
