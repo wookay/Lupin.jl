4d9333f660ed44fd38d075d3ea4009bfaf378ad3
WooKyoung Noh <wookay.noh@gmail.com>
2021-10-04 02:10:21 +0900

    reverse dict

--- src/reverse.jl
+++ src/reverse.jl
@@ -4,4 +4,12 @@ function Base.reverse(nt::NamedTuple{names, T}) where {names, T <: Tuple}
     NamedTuple{reverse(names)}((reverse ∘ values)(nt))
 end
 
+function Base.reverse(dict::Dict{K, V})::Dict{V, K} where {K, V}
+    d = Dict{V, K}()
+    for (k, v) in dict
+        d[v] = k
+    end
+    d
+end
+
 # module Lupin
--- test/lupin/reverse.jl
+++ test/lupin/reverse.jl
@@ -5,4 +5,7 @@ using Lupin
 
 @test reverse((a=1, b=2)) == (b=2, a=1)
 
+d = Dict(:a => 1, :b => 2)
+@test reverse(d) == Dict(1 => :a, 2 => :b)
+
 end # module test_lupin_reverse
